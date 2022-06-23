cb3336140946597dcd5707dc9fd79d5295518e79
marinafischer <marinamirandafischer@gmail.com>
2022-06-23 08:35:07 -0300

    files organization

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const app = express()
 const port = 3000
 const simpsonRoute = require('./Routes/simpsonRoute');
 const loginRoute = require('./Routes/loginRoute');
+const errorMiddleware = require('./middlewares/errorMiddleware');
 
 app.use(express.json());
 
 app.use('/simpsons', simpsonRoute);
 
-app.use('/login', loginRoute)
+app.use('/login', loginRoute);
+
+app.use(errorMiddleware);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ middlewares/errorMiddleware.js
@@ -0,0 +1,7 @@
+module.exports = (err, _req, res, _next) => {
+  if (err.status) {
+    return res.status(err.status).json({ message: err.message });
+  }
+  console.log(err);
+  return res.status(500).json({ message: 'Internal Server Error' });
+};
